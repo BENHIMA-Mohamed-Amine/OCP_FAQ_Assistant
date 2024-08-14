75ea328cce6821ad8aece86d95924ddc68a5d2d2
BENHIMA-Mohamed-Amine <benhima.mohamed.amine@gmail.com>
2024-08-14 18:09:01 +0100

    fix the overflow of the input message in the assistant page

--- front-end/src/components/Chat.jsx
+++ front-end/src/components/Chat.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MessageContainer } from "./MessageContainer";
 
 export const Chat = () => {
+  const [message, setMessage] = useState("");
+  const textareaRef = useRef(null);
+
+  useEffect(() => {
+    adjustTextareaHeight();
+  }, [message]);
+
+  const adjustTextareaHeight = () => {
+    const textarea = textareaRef.current;
+    if (textarea) {
+      if (message === "") {
+        textarea.style.height = "25px";
+        return;
+      }
+
+      textarea.style.height = "auto";
+      textarea.style.height = `${textarea.scrollHeight - 15}px`;
+    }
+  };
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
   return (
-    <div className=" w-5/6 md:w-4/6 mx-auto p-4 flex flex-col justify-between h-[calc(100dvh-80px)]">
+    <div className=" w-5/6 md:w-4/6 mx-auto p-4 flex flex-col justify-between h-[calc(100vh-80px)] overflow-auto">
       <div className="flex-1 overflow-auto">
         <MessageContainer />
         <MessageContainer />
       </div>
       <div>
-        <div className="relative">
-          <input
+        <div className="relative border p-4 ps-8 pe-12 border-gray-300 rounded-lg bg-[#F9F9F9] focus:ring-[hsl(0,0%,90%)] focus:border-[hsl(0,0%,90%)]">
+          <textarea
             type="text"
             id="search"
-            className="text-wrap block w-full p-4 ps-8 text-sm text-main border border-gray-300 rounded-lg bg-[#F9F9F9] focus:ring-[hsl(0,0%,50%)] focus:border-[hsl(0,0%,90%)] focus:outline-none pr-[45px]"
+            ref={textareaRef}
+            value={message}
+            onChange={handleChange}
+            className="text-wrap resize-none block pt-[4px] p-0 w-full text-sm text-main border-none bg-transparent ring-0 focus:ring-0 focus:outline-none"
             placeholder="Message OCP Group Assistant"
             required
-          />
+          ></textarea>
           <button
             type="submit"
-            className="flex justify-center items-center pt-[2px] pr-[1px] absolute end-2.5 bottom-2.5 hover:bg-[hsl(0,0%,90%)] focus:outline-none rounded-lg h-[30px] w-[30px]"
+            className="flex justify-center items-center pt-[2px] pr-[1px] absolute end-2.5 bottom-[50%] transform translate-y-1/2  hover:bg-[hsl(0,0%,90%)] focus:outline-none rounded-lg h-[30px] w-[30px]"
           >
             <img
               src="./src/assets/send.svg"
